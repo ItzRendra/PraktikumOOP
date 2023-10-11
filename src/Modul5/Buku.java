4ec5cdd34bd44b336c4d76c831538c44d50739f2
Raka Rendra Fayanto <l200224246@student.ums.ac.id>
2023-10-11 09:36:34 +0700

    update

--- src/Modul5/Buku.java
+++ src/Modul5/Buku.java
@@ -5,7 +5,7 @@ public class Buku {
     int cetakanKe;
     double hargaJual;
 
-    public Buku1(String pengarang = raka, String judulBuku = "gg", int cetakanKe = 1, double hargaJual =1500.00){
-        System.out.println(pengarang,judulBuku,cetakanKe,hargaJual);
-    }
+    // public Buku1(String pengarang = raka, String judulBuku = "gg", int cetakanKe = 1, double hargaJual =1500.00){
+    //     System.out.println(pengarang,judulBuku,cetakanKe,hargaJual);
+    // }
 }
