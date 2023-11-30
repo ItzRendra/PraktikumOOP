7dce70e7b8ea65e18a793de7930bb121c8f9c9bf
Raka Rendra Fayanto <l200224246@student.ums.ac.id>
2023-11-30 09:01:08 +0700

    modul10

--- src/Modul10/AplikasiLampu.java
+++ src/Modul10/AplikasiLampu.java
@@ -13,15 +13,29 @@ public class AplikasiLampu {
             System.out.println("========================="+
             "\nStatus lampu = " + lampu.statusLampu + "\nKetikkan" +
             "\n0 untuk mematikan lampu \n1 untuk menyalakan lampu \n2 untuk meredupkan lampu");
-            lampu.setSaklar(sc.nextInt());
-            if(lampu.statusLampu == 0){
+            int input = sc.nextInt();
+            // switch (lampu.statusLampu) {
+            //     case 0:
+            //         lampu.matikanLampu();0
+            //     case 1:
+            //         lampu.hidupkanLampu();
+            //     case 2:
+            //         lampu.redupkanLampu();
+            //     default:
+            //     System.out.println("input error");
+            //         break;
+            // }
+            if(input == 0){
                 lampu.matikanLampu();
+                lampu.setSaklar(input);
             }
-            else if (lampu.statusLampu == 1) {
+            else if (input == 1) {
                 lampu.hidupkanLampu();
+                lampu.setSaklar(input);
             }
-            else if (lampu.statusLampu == 2) {
+            else if (input == 2) {
                 lampu.redupkanLampu();
+                lampu.setSaklar(input);
             }
             else{
                 System.out.println("perintah tidak di ketahui");
@@ -31,6 +45,5 @@ public class AplikasiLampu {
             "tekan 9 untuk melanjutkan");
         stop = sc.nextInt();
         }
-        
     }
 }
--- src/Modul10/Lampu.java
+++ src/Modul10/Lampu.java
@@ -8,7 +8,8 @@ public class Lampu implements ActivityLampu{
         if(statusLampu == 0){
             System.out.println("Lampu sudah dalam kondisi mati");
         }
-        else if (statusLampu == 1 && statusLampu ==2) {
+        else {
+            statusLampu = 0;
             System.out.println("Lampu sudah dimatikan");
         }
     }
@@ -16,30 +17,30 @@ public class Lampu implements ActivityLampu{
     @Override
     public void hidupkanLampu(){
         if (statusLampu == 1) {
-            System.out.println("Lampu sudah dihidupkan");
+            System.out.println("Lampu sudah dalam kondisi hidup");
         }
         else {
-            statusLampu += 1 ;
-            System.out.println("Lampu sudah dalam kondisi hidup");
+            statusLampu = 1 ;
+            System.out.println("Lampu sudah dihidupkan");
         }
     }
 
     @Override
     public void redupkanLampu(){
         if (statusLampu == 2) {
-            System.out.println("Lampu sudah diredupkan");
-        }
-        else if(statusLampu == 1){
-            statusLampu += 1;
             System.out.println("Lampu sudah dalam kondisi redup");
         }
         else{
-            statusLampu += 2;
-            System.out.println("Lampu sudah dalam kondisi redup");
+            statusLampu = 2;
+            System.out.println("Lampu sudah diredupkan");
         }
     }
 
     public int setSaklar(int saklar){
         return statusLampu = saklar;
+    }
+
+    public int getStatus(){
+        return statusLampu;
     }
 }
